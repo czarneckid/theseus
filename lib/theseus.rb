9d1f8cb3a0c7fe53eb20e9a545fb03746400b116
David Czarnecki <me@davidczarnecki.com>
2013-11-10 19:46:06 -0500

    Add require for formatter up-front

--- lib/theseus.rb
+++ lib/theseus.rb
@@ -4,3 +4,6 @@ require 'theseus/delta_maze'
 require 'theseus/orthogonal_maze'
 require 'theseus/sigma_maze'
 require 'theseus/upsilon_maze'
+
+require 'theseus/formatters/ascii'
+require 'theseus/formatters/ascii/orthogonal'
